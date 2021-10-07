3a307e5d64fc22d1d41d7e71cc4cdb6aa98e2974
emilie dorer <emiliedorer@gmail.com>
2021-10-07 10:27:55 +0200

    req2

---
+++ requests/jointure/req2.sql
@@ -0,0 +1,4 @@
+SELECT countries.id, name_fr, idperson, lastname, firstname FROM countries 
+INNER JOIN countries_people on countries_people.idcountry = countries.id
+INNER JOIN people on people.id = countries_people.idperson
+WHERE name_fr != 'suisse';
